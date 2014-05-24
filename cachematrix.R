9c1bb1b3e3369b70407f3a7e0a0815abb0e980ce
Tom de Goeij <mail@tomdegoeij.com>
2014-05-24 17:47:30 +0200

    Made adjustments

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A function to create a matrix and store it in the global environment, and a function to display the inverted matrix.
 
-## Write a short comment describing this function
+## Function that lets user define a new matrix and save it in the global environment as 'newmatrix'
 
 makeCacheMatrix <- function(x = matrix()) {
         newmatrix <<- x
 }
 
-## Write a short comment describing this function
+## Function that takes a matrix as input and returns the inverted matrix. Uses inverted matrix from cache if available.
 
 cacheSolve <- function(x, ...) {
+        ## Check if matrices exist. If not the case, then create empty matrices
         if (!exists("originalmatrix")) originalmatrix <- matrix()
         if (!exists("cachedmatrix")) cachedmatrix <- matrix()
-        ## Return a matrix that is the inverse of 'x
+        ## Check if input matrix is the same as the matrix in cache. If so, skip inverting matrix. If not, invert matrix and store in cache.
         if (!identical(originalmatrix,x)){
                 originalmatrix <- x
                 cachedmatrix <- solve(x)
         }
+        ## Return inverted matrix from cache
         return(cachedmatrix)
 }
