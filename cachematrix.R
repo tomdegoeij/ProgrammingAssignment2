18fb34592da5b7263034f0f7ac8f7fd4eac24cb3
Tom de Goeij <mail@tomdegoeij.com>
2014-05-24 18:07:28 +0200

    Made adjustments

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,54 @@
-## A function to create a matrix and store it in the global environment, and a function to display the inverted matrix.
+## A function to store and retrieve the inverted matrix of a given matrix and a function to calculate and display the inverted matrix from cache.
 
-## Function that lets user define a new matrix and save it in the global environment as 'newmatrix'
+## Function that can be used to store and retrieve the inverted matrix of a given matrix in cache
 makeCacheMatrix <- function(x = matrix()) {
-        newmatrix <<- x
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 ## Function that takes a matrix as input and returns the inverted matrix. Uses inverted matrix from cache if available.
 cacheSolve <- function(x, ...) {
-        ## Check if matrices exist. If not the case, then create empty matrices
-        if (!exists("originalmatrix")) originalmatrix <- matrix()
-        if (!exists("cachedmatrix")) cachedmatrix <- matrix()
-        ## Check if input matrix is the same as the matrix in cache. If so, skip inverting matrix. If not, invert matrix and store in cache.
-        if (!identical(originalmatrix,x)){
-                originalmatrix <- x
-                cachedmatrix <- solve(x)
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
         }
-        ## Return inverted matrix from cache
-        return(cachedmatrix)
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+}
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
+cacheSolve <- function(x, ...) {
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
